968542a707b3d707c5063b32b06a35932c3a125b
AB404012 <soswin88@yahoo.com>
2023-02-18 23:19:11 -0800

    alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,15 +2,15 @@
 
 /**
  * main - prints the alphabet in lowercase
- * Followed by a new line, except q & e
- * Return: Always 0 (success)
+ * followed by a new line, except q and e
+ * Return: Always 0
  */
 int main(void)
 {
 	char la;
 
 	for (la = 'a'; la <= 'z'; la++)
-        {
+	{
 		if (la != 'e' && la != 'q')
 			putchar(la);
 	}
