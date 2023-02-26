0693407741bc34eb1f99811cc18b6e7b86deaeaa
AB404012 <soswin88@yahoo.com>
2023-02-26 02:10:15 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-
-int main(void)
-{
-	char c;
-	
-	c = 'a';
-	while
-		(c <= 'z')
-	{
-	             if (( c |= 'q' && c |= 'e') && c <= 'z')
-			putchar(c);
-		        c++;
-	}
-	putchar('\n');
-=======
 
 /**
  * main - prints the alphabet in lowercase
@@ -38,6 +17,5 @@ int main(void)
 
 	putchar('\n');
 
->>>>>>> c03aa3aa69208c5403005f4aa7ee1d83e28c3562
 	return (0);
 }
